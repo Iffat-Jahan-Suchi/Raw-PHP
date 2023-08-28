c12af4a82eb7e4c01e2367fbbecd67d8190dd1b8
Iffat Jahan <iffatjahan046@gmail.com>
2023-08-28 23:35:10 +0600

    search

--- insert/search.php
+++ insert/search.php
@@ -1,5 +1,20 @@
 <?php
+$con = mysqli_connect('localhost', 'root', '', 'test');
+if (isset($_REQUEST['submit'])) {
+    $name = $_REQUEST['name'];
+    $query = "SELECT * FROM users WHERE user_name='$name'";
+    $result=mysqli_query($con,$query);
+    if($raw=mysqli_fetch_assoc($result))
+    {
+        $name=$raw['name'];
+        echo $name;
+    }
+
+}
 ?>
 
 
-<input type="text" name="name" placeholder="enter search user">
+<form action="" method="post">
+    <input type="text" name="name" placeholder="enter search user">
+    <input type="submit" name="search">
+</form>
